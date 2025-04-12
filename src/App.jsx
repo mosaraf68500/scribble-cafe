e92e90512b98937e4a17723f1bb73b581c2a9cf2
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-12 22:29:32 +0600

    data load

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import Blogs from "./components/Blogs/Blogs";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Navbar></Navbar>
+
+      <div className="container flex text-center ">
+        <div className="left-side w-[70%]">
+          <Blogs></Blogs>
+        </div>
+        <div className="right-side w-[30%]">
+
+          <h1>Reading time:0</h1>
+          <h1>BookmarkedCount:0</h1>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Blogs/Blogs.jsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+const Blogs = () => {
+  const [blogs, setBlogs] = useState([]);
+
+  useEffect(() => {
+    fetch("blogs.json")
+      .then((res) => res.json())
+      .then((data) => setBlogs(data));
+  }, []);
+
+  // console.log(blogs)
+  return (
+    <div>
+      <h1 className="text-3xl font-semibold">Total:{blogs.length}</h1>
+    </div>
+  );
+};
+
+export default Blogs;
