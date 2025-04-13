17aa39809b222b9c458104efbf832aefbb2ae38a
Md:Mosaraf Hossen <151431976+mosaraf68500@users.noreply.github.com>
2025-04-13 08:51:52 +0600

    bookmark

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { FaRegBookmark } from "react-icons/fa";
+
 
 const Blog = ({blog}) => {
     const { title, cover,author_img,author,posted_date,reading_time,hashtags}=blog;
@@ -11,8 +13,21 @@ const Blog = ({blog}) => {
       alt="Shoes"
       className="rounded-xl" />
   </figure>
+
+  <div className="flex justify-around pt-2 items-center">
+    <h3>{author} </h3>
+    <img className="w-10" src={author_img} alt="" />
+    <FaRegBookmark size={30} />
+
+  </div>
   <div className="card-body items-center text-center">
     <h2 className="card-title">{title}</h2>
+<div className="flex justify-around items-center gap-6">
+{
+    hashtags.map(has=><p>{has}</p>)
+}
+</div>
+
     <div className="card-actions">
       <button className="btn btn-primary">reading as mark</button>
     </div>
